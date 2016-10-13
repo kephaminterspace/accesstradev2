36da7fb7ba34b49066b072322e03bd4fc4554826
mebao <ke.v.pham@vntrip.vn>
2016-10-13 09:14:03 +0700

    Issue - Optimize: Minify and remove consolog.

--- contents/advertiser/header.php
+++ contents/advertiser/header.php
@@ -39,7 +39,7 @@
                                     <li><a href="<?php echo esc_url(home_url( '/' )) ;?>huong-dan-advertiser">Hướng dẫn</a></li>
                                     <li><a href="<?php echo esc_url(home_url( '/' )) ;?>blog">Blog</a></li>
                                     <li><a href="http://forum.accesstrade.vn">Forum</a></li>
-                                    <li class="navbar-nav-btn"><a href="https://member.accesstrade.vn/m/merchantlogin" target="_blank">Đăng nhập</a></li>
+                                    <li class="navbar-nav-btn"><a href="https://pub.accesstrade.vn" target="_blank">Đăng nhập</a></li>
                                     <li class="hidden-sm hidden-md hidden-lg nav-btn-blue"><a href="<?php echo esc_url(home_url( '/' )) ;?>">Publisher</a></li>
                                 </ul>
                             </div>
