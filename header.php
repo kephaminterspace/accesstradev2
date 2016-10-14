7103deb31fa9edc2977349e4d8529c3090ee2ce2
mebao <ke.v.pham@vntrip.vn>
2016-10-14 15:41:48 +0700

    anh Kế ơi anh cài mã pixcel [14:54:09] Hòa Blue - ACCESSTRADE Việt Nam: này vào website accesstrade.vn cho em với ạ [14:54:17] Hòa Blue - ACCESSTRADE Việt Nam: em sẽ tạo task sau ạ

--- header.php
+++ header.php
@@ -16,8 +16,8 @@
     <meta itemprop="ratingValue" content="10">
     <meta itemprop="bestRating" content="9">
     <meta itemprop="worstRating" content="0">
-    <meta itemprop="ratingCount" content="200"></span>
-
+    <meta itemprop="ratingCount" content="200">
+    </span>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title( '|', true, 'right' ); ?></title>
@@ -36,6 +36,22 @@
 </head>
 
 <body>
+<!-- Facebook Pixel Code -->
+<script>
+    !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+        n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+        document,'script','https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '625592474247286');
+    fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+               src="https://www.facebook.com/tr?id=625592474247286&ev=PageView&noscript=1"
+    /></noscript>
+<!-- DO NOT MODIFY -->
+<!-- End Facebook Pixel Code -->
+
 <?php
 if ( is_home() ) {
     // This is the blog posts index
